b48a3c67005b8722925f41b290de6f1b7ecd4264
Jeff <rustyangel@gmail.com>
2022-02-09 10:05:22 -0700

    Chore: Use proper type for ESLint config settings

--- zazen.config.ts
+++ zazen.config.ts
@@ -1,15 +1,16 @@
-type EsLintConfig = Record<string, unknown>
+/* eslint-disable import/no-extraneous-dependencies */
+import { Linter } from 'eslint'
 
 export default {
-	setEslintConfig: (config: EsLintConfig) => ({
+	setEslintConfig: (config: Linter.Config) => ({
 		...config,
 		extends: [
-			...((config.extends as string[]) ?? []),
+			...(config.extends as string[]),
 			'@zazen/eslint-config/node',
 			'@zazen/eslint-config/typescript',
 		],
 		rules: {
-			...((config.rules as Record<string, unknown>) ?? []),
+			...config.rules,
 			'@typescript-eslint/no-require-imports': 'off',
 			'@typescript-eslint/no-unsafe-return': 'off',
 			'import/no-anonymous-default-export': [
